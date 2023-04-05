b6acd2d8f2565d8086e7f9da6c28a4f5e3afc628
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-05 15:54:44 -0400

    set up associations and fields in models

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// create a connnection object
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3301,
       dialectOptions: {
         decimalNumbers: true,
       },
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
+const sequelize = require('./config/connection')
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Connect to database before starting Express.js server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
 
 app.use(routes);
 
